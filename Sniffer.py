d9e417c05cfc927768dd13ea5c0304812e1ff9ba
Spagecko <bennettlawrenz@gmail.com>
2019-06-10 19:55:55 -0700

    Add files via upload

--- Sniffer.py
+++ Sniffer.py
@@ -12,7 +12,7 @@ s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
 
 # receive a packet
 while True:
-	print("no")
+	
 	packet= s.recvfrom(65565)
-	print("do i work")
+	
 	print (packet)
