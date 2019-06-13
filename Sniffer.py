8996a0d223943e1fe3834bc729f872f461ace7b1
mstfasaid94 <35977215+mstfasaid94@users.noreply.github.com>
2019-06-12 20:11:54 -0700

    Update Sniffer.py

--- Sniffer.py
+++ Sniffer.py
@@ -6,6 +6,36 @@ import struct
 import binascii
 import os
 
+class unpack: 
+
+ def __cinit__(self): 
+   self.data=None
+
+ #Ethernet Header 
+ def ethernet(self, data): 
+   store=data
+   store=struct.unpack("!6s6sH",store)
+   dest_mac=binascii.hexlify(store[0])
+   src_mac=binascii.hexlify(store[1])
+   ethernet_protocol=store[2]
+   data={"Dest_Mac_addr":dest_mac,
+   "Src_Mac_addr" :src_mac, 
+   "Protocol" : ethernet_protocol} 
+   return data 
+
+  # ICMP HEADER Extraction 
+  def icmp(self,data): 
+   icmph=struct.unpack( '!BBH', data) 
+   icmpType=icmph[0]
+   code = icmph[1]
+   chk_sum = icmph[2]
+   data={ 'ICMP TYPE' :icmpType, 
+   "Code" :code,
+   "CheckSum":chk_sum}
+   return data  
+
+
+
 
 #create an INET, raw socket
 s=socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0800))
